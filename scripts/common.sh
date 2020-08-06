978df3b89dce75fe141c677937f85d82c3ab49ef
Yi Lin <qinsoon@gmail.com>
2020-08-06 11:59:36 +1000

    Refactor JikesRVM scripts

---
+++ scripts/common.sh
@@ -0,0 +1,80 @@
+# root dir of this perf kit
+kit_root=$(realpath $(dirname "$0")/..)
+# where we put all the builds
+kit_build=$kit_root/running/build
+# where we put all the scripts
+kit_script=$kit_root/scripts
+# where we put all the configs
+config_dir=$kit_root/configs
+# where result logs are stored
+log_dir=$kit_root/running/results/log
+# where we put results
+result_repo_dir=$kit_root/result_repo
+
+# build_jikesrvm 'binding_path' 'plan' 'build_path'
+# Env: JAVA_HOME
+build_jikesrvm_with_mmtk() {
+    binding_path=$1
+    plan=$2
+    build_path=$3 # put the build here
+
+    jikesrvm_path=$binding_path/repos/jikesrvm
+
+    cd $jikesrvm_path
+
+    # build
+    python scripts/testMMTk.py -g $plan -j $JAVA_HOME --build-only -- -quick --answer-yes --use-third-party-heap=../.. --use-third-party-build-configs=../../jikesrvm/build/configs --use-external-source=../../jikesrvm/rvm/src
+
+    # copy to build_path
+    cp -r $jikesrvm_path'/dist/'$plan'_x86_64-linux' $build_path/
+}
+
+# run_benchmarks 'config'
+run_benchmarks() {
+    config=$1
+
+    cp $config $kit_root/running/bin/RunConfig.pm
+    output=$($kit_root/running/bin/runbms 16 16)
+    run_id=$(echo $output | cut -d ' ' -f 3) # output is something like: 'Run id: fox-2020-05-13-Wed-124656'
+
+    echo $run_id
+}
+
+# prepare_dir 'path'
+# Make sure the dir exists and is empty
+ensure_empty_dir() {
+    path=$1
+
+    mkdir -p $path
+    rm -rf $path/*
+}
+
+# start_venv 'venv_path'
+start_venv() {
+    venv_path=$1
+
+    python3 -m venv $venv_path
+    source $venv_path/bin/activate
+}
+
+leave_venv() {
+    deactivate
+}
+
+# Env: RESULT_REPO_ACCESS_TOKEN, RESULT_REPO, RESULT_REPO_BRANCH
+checkout_result_repo() {
+    rm -rf $result_repo_dir
+    # Use this for local testing
+    # git clone ssh://git@github.com/$RESULT_REPO.git $result_repo_dir --branch=$RESULT_REPO_BRANCH
+    git clone https://$RESULT_REPO_ACCESS_TOKEN@github.com/$RESULT_REPO.git $result_repo_dir --branch=$RESULT_REPO_BRANCH
+}
+
+# commit_result_repo_dir 'message'
+commit_result_repo() {
+    message=$1
+
+    git --git-dir $result_repo_dir/.git add .
+    git --git-dir $result_repo_dir/.git commit -m $message
+    git --git-dir $result_repo_dir/.git pull --rebase # pull any new commit (if any)
+    git --git-dir $result_repo_dir/.git push
+}
--- scripts/jikesrvm-compare.sh
+++ scripts/jikesrvm-compare.sh
@@ -1,4 +1,8 @@
 set -ex
+
+# include common.sh
+. $(dirname "$0")/common.sh
+
 jikesrvm_binding_trunk=$(realpath $1)
 mmtk_core_trunk=$(realpath $2)
 jikesrvm_binding_branch=$(realpath $3)
@@ -13,69 +17,45 @@ mmtk_branch_rev=$(git -C $mmtk_core_branch rev-parse HEAD)
 # JikesRVM root
 jikesrvm_trunk=$jikesrvm_binding_trunk/repos/jikesrvm
 jikesrvm_branch=$jikesrvm_binding_branch/repos/jikesrvm
-# root dir of this perf kit
-kit_root=$(realpath $(dirname "$0")/..)
-# where we put all the builds
-kit_build=$kit_root/running/build
-mkdir -p $kit_build
-rm -rf $kit_build/*
 
 # Build - JikesRVM buildit script requires current dir to be JikesRVM root dir
+ensure_empty_dir $kit_build
 
 # Trunk
-cd $jikesrvm_trunk
-# Build for trunk
 rsync -avLe $mmtk_core_trunk/* $jikesrvm_binding_trunk/repos/mmtk-core
-# NoGC
-python scripts/testMMTk.py -g RFastAdaptiveNoGC -j $JAVA_HOME --build-only -- -quick --answer-yes --use-third-party-heap=../.. --use-third-party-build-configs=../../jikesrvm/build/configs --use-external-source=../../jikesrvm/rvm/src
-cp -r $jikesrvm_trunk/dist/RFastAdaptiveNoGC_x86_64-linux $kit_build/NoGC_Trunk_x86_64-linux/
-# SemiSpace
-python scripts/testMMTk.py -g RFastAdaptiveSemiSpace -j $JAVA_HOME --build-only -- -quick --answer-yes --use-third-party-heap=../.. --use-third-party-build-configs=../../jikesrvm/build/configs --use-external-source=../../jikesrvm/rvm/src
-cp -r $jikesrvm_trunk/dist/RFastAdaptiveSemiSpace_x86_64-linux $kit_build/SemiSpace_Trunk_x86_64-linux/
+build_jikesrvm_with_mmtk $jikesrvm_binding_trunk $mmtk_core_trunk RFastAdaptiveNoGC $kit_build/NoGC_Trunk_x86_64-linux
+build_jikesrvm_with_mmtk $jikesrvm_binding_trunk $mmtk_core_trunk RFastAdaptiveSemiSpace $kit_build/SemiSpace_Trunk_x86_64-linux
 
 # Branch
-cd $jikesrvm_branch
-# Build for branch
 rsync -avLe $mmtk_core_branch/* $jikesrvm_binding_branch/repos/mmtk-core
-# NoGC
-python scripts/testMMTk.py -g RFastAdaptiveNoGC -j $JAVA_HOME --build-only -- -quick --answer-yes --use-third-party-heap=../.. --use-third-party-build-configs=../../jikesrvm/build/configs --use-external-source=../../jikesrvm/rvm/src
-cp -r $jikesrvm_branch/dist/RFastAdaptiveNoGC_x86_64-linux $kit_build/NoGC_Branch_x86_64-linux/
-# SemiSpace
-python scripts/testMMTk.py -g RFastAdaptiveSemiSpace -j $JAVA_HOME --build-only -- -quick --answer-yes --use-third-party-heap=../.. --use-third-party-build-configs=../../jikesrvm/build/configs --use-external-source=../../jikesrvm/rvm/src
-cp -r $jikesrvm_branch/dist/RFastAdaptiveSemiSpace_x86_64-linux $kit_build/SemiSpace_Branch_x86_64-linux/
+build_jikesrvm_with_mmtk $jikesrvm_binding_branch $mmtk_core_branch RFastAdaptiveNoGC $kit_build/NoGC_Branch_x86_64-linux
+build_jikesrvm_with_mmtk $jikesrvm_binding_branch $mmtk_core_branch RFastAdaptiveSemiSpace $kit_build/SemiSpace_Branch_x86_64-linux
 
 # Run
 cd $kit_root
-mkdir -p $kit_root/running/bin/probes
+ensure_empty_dir $kit_root/running/bin/probes
 cp $kit_root/probes/probes.jar $kit_root/running/bin/probes/
 
 echo "JikesRVM" >> $output_file
 echo "====" >> $output_file
 
-echo "* binding_trunk: [$jikesrvm_binding_trunk](https://github.com/mmtk/mmtk-jikesrvm/commit/$jikesrvm_trunk_rev)" >> $output_file
+echo "* binding_trunk: [$jikesrvm_trunk_rev](https://github.com/mmtk/mmtk-jikesrvm/commit/$jikesrvm_trunk_rev)" >> $output_file
 echo "* trunk:   [$mmtk_trunk_rev](https://github.com/mmtk/mmtk-core/commit/$mmtk_trunk_rev)" >> $output_file
-echo "* binding_branch: [$jikesrvm_binding_branch](https://github.com/mmtk/mmtk-jikesrvm/commit/$jikesrvm_branch_rev)" >> $output_file
+echo "* binding_branch: [$jikesrvm_branch_rev](https://github.com/mmtk/mmtk-jikesrvm/commit/$jikesrvm_branch_rev)" >> $output_file
 echo "* branch:  [$mmtk_branch_rev](https://github.com/mmtk/mmtk-core/commit/$mmtk_branch_rev)" >> $output_file
 
 echo "" >> $output_file
 
 # Python venv
-python3 -m venv python-env
-source python-env/bin/activate
-pip3 install -r scripts/requirements.txt
+start_venv python-env
+pip3 install -r $(dirname "$0")/requirements.txt
 
 # Run for NoGC
-cp $kit_root/configs/RunConfig-JikesRVM-NoGC-FastCompare.pm $kit_root/running/bin/RunConfig.pm
-nogc_output=$($kit_root/running/bin/runbms 16 16)
-nogc_run_id=$(echo $nogc_output | cut -d ' ' -f 3) # output is something like: 'Run id: fox-2020-05-13-Wed-124656'
-
+nogc_run_id=$(run_benchmarks $kit_root/configs/RunConfig-JikesRVM-NoGC-FastCompare.pm)
 # Result for NoGC
 python $kit_root/scripts/compare_report.py $kit_root/running/results/log/$nogc_run_id NoGC NoGC_Trunk NoGC_Branch 40 >> $output_file
 
 # # Run for SemiSpace
-cp $kit_root/configs/RunConfig-JikesRVM-SemiSpace-FastCompare.pm $kit_root/running/bin/RunConfig.pm
-ss_output=$($kit_root/running/bin/runbms 16 16)
-ss_run_id=$(echo $ss_output | cut -d ' ' -f 3) # output is something like: 'Run id: fox-2020-05-13-Wed-124656'
-
+ss_run_id=$(run_benchmarks $kit_root/configs/RunConfig-JikesRVM-SemiSpace-FastCompare.pm)
 # # Result for SemiSpace
 python $kit_root/scripts/compare_report.py $kit_root/running/results/log/$ss_run_id SemiSpace SemiSpace_Trunk SemiSpace_Branch 40 >> $output_file
