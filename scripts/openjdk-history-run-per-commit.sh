ef214bb03c83c57c4cab0deac258ea34c989c3d8
Yi Lin <qinsoon@gmail.com>
2021-11-12 09:35:44 +1100

    store logs to a separate dir. Renamer jikesrvm-history-run.sh to jikesrvm-history-run-per-commit.sh

--- scripts/openjdk-history-run-per-commit.sh
+++ scripts/openjdk-history-run-per-commit.sh
@@ -19,7 +19,7 @@ build_openjdk_with_mmtk $openjdk_binding release $kit_build/jdk-mmtk
 
 # --- Run ---
 cd $kit_root
-saved_log_dir=$result_repo_dir/openjdk/per_commit
+saved_log_dir=$result_repo_dir/openjdk_per_commit
 
 # Run For NoGC
 nogc_run_id=$(run_benchmarks_custom_heap $log_dir $kit_root/configs-ng/openjdk/history/nogc.yml $history_invocations)
@@ -45,4 +45,4 @@ commit_result_repo 'OpenJDK Binding: '$openjdk_rev
 # plot result
 ensure_empty_dir $output_dir
 cd $kit_root
-python3 scripts/history_report.py configs/openjdk-plot.yml $result_repo_dir/openjdk $result_repo_dir/openjdk_stock $output_dir
+python3 scripts/history_report.py configs/openjdk-plot.yml $saved_log_dir $result_repo_dir/openjdk_stock $output_dir
