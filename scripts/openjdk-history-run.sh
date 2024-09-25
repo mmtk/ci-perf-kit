128a679d9c7d650c52e9a4037ce5b9cc7926b522
Kunshan Wang <wks1986@gmail.com>
2024-09-25 20:52:43 +0800

    Add canary for openjdk regression test
    The canary will be run alongside the newest revision in regression test
    to identify unnoticed environment changes.

--- scripts/openjdk-history-run.sh
+++ scripts/openjdk-history-run.sh
@@ -3,12 +3,14 @@ set -ex
 # include common.sh
 . $(dirname "$0")/common.sh
 
-openjdk_binding=$(realpath $1)
-output_dir=$(realpath -m $2)
-openjdk_rev=$(git -C $openjdk_binding rev-parse HEAD)
+openjdk_binding_latest=$(realpath $1)
+openjdk_binding_canary=$(realpath $2)
+output_dir=$(realpath -m $3)
+# openjdk_rev is used for the commit message.  We use the revision ID of the latest version.
+openjdk_rev=$(git -C $openjdk_binding_latest rev-parse HEAD)
 
 # OpenJDK root
-openjdk=$openjdk_binding/repos/openjdk
+openjdk=$openjdk_binding_latest/repos/openjdk
 
 ensure_empty_dir $kit_build
 ensure_empty_dir $kit_upload
@@ -16,18 +18,20 @@ ensure_empty_dir $log_dir
 checkout_result_repo
 
 # Build
-build_openjdk_with_mmtk $openjdk_binding release jdk-mmtk
+build_openjdk_with_mmtk $openjdk_binding_latest release jdk-mmtk
+build_openjdk_with_mmtk $openjdk_binding_canary release jdk-mmtk-canary
 
 run_exp() {
-    plan=$1
+    dir_name=$1
     config=$2
     heap_modifier=$3
 
     # Run
     run_id=$(run_benchmarks $log_dir $kit_root/configs/$config $heap_modifier $history_invocations)
     # Save result
-    mkdir -p $result_repo_dir/openjdk/$plan
-    cp -r $log_dir/$run_id $result_repo_dir/openjdk/$plan
+    RESULT_DIR=$result_repo_dir/openjdk/$dir_name
+    mkdir -p $RESULT_DIR
+    cp -r $log_dir/$run_id $RESULT_DIR
 }
 
 # Build
@@ -53,6 +57,11 @@ run_exp stickyimmix running-openjdk-stickyimmix-complete.yml 6
 
 # MarkSweep
 run_exp marksweep running-openjdk-marksweep-complete.yml 6
+
+# GenImmix using the canary version.
+# If the performance of the canary version changed,
+# it means there is an environment change that impacts the performance.
+run_exp canary running-openjdk-canary-complete.yml 6
 
 # Commit result
 commit_result_repo 'OpenJDK Binding: '$openjdk_rev
